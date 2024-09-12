b31d3b2c2f6fc43fe0d6f7f18dc29c11af166a3e
exaFLOPsDenny <121441689+exaFLOPsDenny@users.noreply.github.com>
2024-09-12 14:31:44 +0900

    Add files via upload

---
+++ InPerson/main.py
@@ -0,0 +1,10 @@
+#Algorithm 1: Sampling-Based MPC
+
+# Number of samples
+K = 1000
+
+# Number of time-steps
+# But my opinion is that this number should be dynamic
+
+# Static Number of time steps
+T = 50
